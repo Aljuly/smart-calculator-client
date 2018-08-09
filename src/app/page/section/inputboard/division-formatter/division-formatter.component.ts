4be92621c6dab945ac8751ad2c97d5780260a186
alexander.zhulinsky <alzhulinsky@gmail.com>
2018-08-09 07:01:29 +0300

    division-formatter fixes

--- src/app/page/section/inputboard/division-formatter/division-formatter.component.ts
+++ src/app/page/section/inputboard/division-formatter/division-formatter.component.ts
@@ -1,15 +1,27 @@
 import { Step } from '../../../../_models';
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-result',
   templateUrl: './division-formatter.component.html',
   styleUrls: ['./division-formatter.component.css']
 })
-export class DivisionFormatterComponent {
-  dividend: string;
-  divisor: string;
-  quotient: string;
-  remainder: string;
-  steps: Step[];
+export class DivisionFormatterComponent implements OnInit {
+  @Input() dividend: string;
+  @Input() divisor: string;
+  @Input() quotient: string;
+  @Input() remainder: string;
+  @Input() steps: Step[];
+
+  constructor() {}
+
+  ngOnInit() {
+    let j = 0;
+    let diff = 0;
+    let i = 0;
+    for (let step: Step in this.steps) {
+      i++;
+      
+    }
+  }
 }
